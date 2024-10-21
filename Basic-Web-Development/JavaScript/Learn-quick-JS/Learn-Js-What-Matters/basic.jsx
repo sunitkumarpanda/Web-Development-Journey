b071a62b1c0e09b93ecf3c5eeb83f1c7b11766be
Sunit Ku. Panda <sunitpanda680@gmail.com>
2024-10-21 20:13:20 +0530

    Push

---
+++ Basic-Web-Development/JavaScript/Learn-quick-JS/Learn-Js-What-Matters/basic.jsx
@@ -0,0 +1,7 @@
+const myElement = (
+    <ul>
+      <li>Apples</li>
+      <li>Bananas</li>
+      <li>Cherries</li>
+    </ul>
+  );
---
+++ Basic-Web-Development/JavaScript/Learn-quick-JS/Learn-Js-What-Matters/script.js
@@ -0,0 +1,49 @@
+// Word v/s keyword
+// var, const, --let
+// Variable & Constants
+const dulha = "Raj";
+var dulhan = "Rajasri";
+// Hoisting
+// types in js --primitive, reference- [] () {}.
+var a = [12,13];
+var b = a;
+b.pop();
+// conditionals if else, else-if.
+if (10>4) {
+
+}
+else if (10>3) {
+
+}
+else{
+
+}
+// Loops - for, while.
+for (var i = 25; i <= 50; i++){
+    console.log(i);
+}
+
+var a = 12;
+while(a<=20){
+    a++;
+}
+// function
+function hellobolo(){
+    console.log("Hello");
+}
+hellobolo();
+
+function xyz(a,b,c){
+    console.log(a,b,c);
+
+}
+xyz(1,2,3);
+// Arrays
+var arr = [2,4,6,8];
+arr[0];
+arr.push(3);
+arr.pop();
+arr.unshift(0);
+arr.shift();
+arr.splice(1, 2);
+// objects
